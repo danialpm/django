b45327835f8eb43d2c920082351bbe2a5b8160ff
danial pm <pm.danial@gmail.com>
2021-07-01 04:16:31 +0430

    add tags

--- shoes/blog/views.py
+++ shoes/blog/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Post
+from .models import Post, Comment
 from django.core.paginator import Paginator,EmptyPage,PageNotAnInteger
-from .forms import EmailPostForm
+from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
+from taggit.models import Tag
 
 
-def post_list(request):
+def post_list(request, tag_slug=None):
     object_list = Post.objects.filter(status='published')
+    tag = None
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        object_list = object_list.filter(tags__in=[tag])
     paginator = Paginator(object_list, 2) # 3 posts in each page
     page = request.GET.get('page')
     try:
@@ -20,7 +25,8 @@ def post_list(request):
     return render(request,
         'blog/post/list.html',
         {'page': page,
-        'posts': posts})
+        'posts': posts,
+        'tag': tag})
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, slug=post,
@@ -28,11 +34,29 @@ def post_detail(request, year, month, day, post):
         publish__year=year,
         publish__month=month,
         publish__day=day)
+    # List of active comments for this post
+    comments = post.comments.filter(active=True)
+    new_comment = None
+    if request.method == 'POST':
+        # A comment was posted
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            # Create Comment object but don't save to database yet
+            new_comment = comment_form.save(commit=False)
+            # Assign the current post to the comment
+            new_comment.post = post
+            # Save the comment to the database
+            new_comment.save()
+    else:
+        comment_form = CommentForm()
     post.visits = post.visits +1
     post.save()
     return render(request,
         'blog/post/detail.html',
-        {'post': post})
+        {'post': post,
+        'comments': comments,
+        'new_comment': new_comment,
+        'comment_form': comment_form})
 
 
 def post_share(request, post_id):
